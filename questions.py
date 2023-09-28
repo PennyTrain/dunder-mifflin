794a95f843787093278c1d7fb3bc35199942d227
Penny Train <penny.train04@gmail.com>
2023-09-28 21:35:07 +0100

    Add and finish questions to the quiz.

---
+++ questions.py
@@ -0,0 +1,139 @@
+office_questions = [
+    {
+        'question': 'What item of Dwights did Jim put into jello?:',
+        'answers': ['Pen', 'Phone', 'Stapler'],
+        'correct': 'Stapler'
+    },
+    {
+        'question': 'What game does Angela tell Pam she likes to play in the office?:',
+        'answers': ['Jim Ball', 'Pam Pong', 'Micheals Mission'],
+        'correct': 'Pam Pong'
+    },
+    {
+        'question': 'What item of Andys did Jim put in jello?:',
+        'answers': ['Wallet', 'Stapler', 'Calculator'],
+        'correct': 'Calculator'
+    },
+    {
+        'question': 'How many years were Roy and Pam engaged for:',
+        'answers': ['1', '2', '3'],
+        'correct': '3'
+    },
+    {
+        'question': 'What word did Dwight have taped to his head for "Diversity Day":',
+        'answers': ['Black', 'Asian', 'Jewish'],
+        'correct': 'Asian'
+    },
+    {
+        'question': 'What is the perfume that Phylis wears made out of?:',
+        'answers': ['Cedar', 'Real Pine', 'Sandal Wood'],
+        'correct': 'Real Pine'
+    },
+    {
+        'question': 'What is Gabes real name?:',
+        'answers': ['Susan', 'Gabriel', 'Lee'],
+        'correct': 'Susan'
+    },
+    {
+        'question': 'What part of his body does Micheal burn on a George Foreman grill:',
+        'answers': ['Hand', 'Tongue', 'Foot'],
+        'correct': 'Foot'
+    },
+    {
+        'question': 'What serial killer is mentioned throughout the series:',
+        'answers': ['Scranton Strangler', 'Killer Kree', 'Violent Vic'],
+        'correct': 'Scranton Strangler'
+    },
+    {
+        'question': 'Where did Jim and Pam go on their honey moon?:',
+        'answers': ['Italy', 'Spain', 'Puerto Rico'],
+        'correct': 'Puerto Rico'
+    },
+    {
+        'question': 'Who is Angela and Dwights sons godfather?:',
+        'answers': ['Senator Lipton', 'Oscar', 'Jim'],
+        'correct': 'Oscar'
+    },
+    {
+        'question': 'Which of the following is a quote from Stanley?:',
+        'answers': ['Did I stutter?', 'Did I sneeze?', 'Did I shout?'],
+        'correct': 'Did I stutter'
+    },
+    {
+        'question': 'What kind of animal got loose in the office?:',
+        'answers': ['Pigeon', 'Bat', 'Cat'],
+        'correct': 'Bat'
+    },
+    {
+        'question': 'How many cats does Angela have:',
+        'answers': ['13', '17', '23'],
+        'correct': '17'
+    },
+    {
+        'question': 'How many seasons was Micheal Scott on The Office:',
+        'answers': ['5', '6', '7'],
+        'correct': '7'
+    },
+    {
+        'question': 'What was Stanleys wifes name',
+        'answers': ['Annie', 'Teri', 'Cheryl'],
+        'correct': 'Teri'
+    },
+    {
+        'question': 'Where is The Office set?',
+        'answers': ['Scranton', 'Hunstvile', 'The Woodlands'],
+        'correct': 'Scranton'
+    },
+    {
+        'question': 'How many children did Jim and Pam have?:',
+        'answers': ['2', '3', '4'],
+        'correct': '2'
+    },
+    {
+        'question': 'What was Dwights Farm named?:',
+        'answers': ['Schrute Farms', 'Beet Project', 'Mo and Dwight'],
+        'correct': 'Schrute Farms'
+    },
+    {
+        'question': 'Where did Roy take Pam on their first date?:',
+        'answers': ['Minor league hockey game', 'The movies', 'A nice dinner'],
+        'correct': 'Minor league hockey game'
+    },
+    {
+        'question': 'What is the name of Creeds blog?:',
+        'answers': ['My naughty list', 'Creed thoughts', 'Dear Diary'],
+        'correct': 'Creed thoughts'
+    },
+    {
+        'question': 'What does Pam steal in the first season?:',
+        'answers': ['Staples', 'Post it notes', 'Paper clips'],
+        'correct': 'Post it notes'
+    },
+    {
+        'question': 'Who dates Pams mother?',
+        'answers': ['Creed', 'Micheal', 'Toby'],
+        'correct': 'Micheal'
+    },
+    {
+        'question': 'What does Dwights family farm produce?:',
+        'answers': ['Potatoes', 'Carrots', 'Beets'],
+        'correct': 'Beets'
+    },
+    {
+        'question': 'Who bought Micheals "Worlds Best Boss" mug:',
+        'answers': ['Dwight', 'Jim', 'Micheal'],
+        'correct': 'Micheal'
+    },
+    {
+        'question': 'What food is Kevin forced to eat?',
+        'answers': ['Beets', 'Broccoli', 'Potatoe'],
+        'correct': 'Broccoli'
+    },
+    {
+        'question': 'Where does Jim propose to Pam??',
+        'answers': ['A gas station',
+                    'A fancy dinner',
+                    'At the fireworks'],
+        'correct': 'A gas station'
+    }
+]
