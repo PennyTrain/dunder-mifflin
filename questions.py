450d3cc2510801cc8ddb95911bff87ed3afed194
Penny Train <penny.train04@gmail.com>
2023-10-11 14:17:32 +0100

    Edit questions.py fixing syntax errors and line length after running it through a validator

--- questions.py
+++ questions.py
@@ -1,190 +1,193 @@
 office_questions = [
     {
         'question': 'What item of Dwights did Jim put into jello?:',
-        'answers': ['a) Pen', 
-                    'b) Phone', 
+        'answers': ['a) Pen',
+                    'b) Phone',
                     'c) Stapler'],
         'correct': 'c'
     },
     {
-        'question': 'What game does Angela tell Pam she likes to play in the office?:',
-        'answers': ['a) Jim Ball', 
-                    'b) Pam Pong', 
+        'question': """What game does Angela tell Pam she
+        likes to play in the office?:""",
+        'answers': ['a) Jim Ball',
+                    'b) Pam Pong',
                     'c) Micheals Mission'],
         'correct': 'b'
     },
     {
         'question': 'What item of Andys did Jim put in jello?:',
-        'answers': ['a) Wallet', 
-                    'b) Stapler', 
+        'answers': ['a) Wallet',
+                    'b) Stapler',
                     'c) Calculator'],
         'correct': 'c'
     },
     {
         'question': 'How many years were Roy and Pam engaged for:',
-        'answers': ['a) 1', 
-                    'b) 2', 
+        'answers': ['a) 1',
+                    'b) 2',
                     'c) 3'],
         'correct': 'c'
     },
     {
-        'question': 'What word did Dwight have taped to his head for "Diversity Day":',
-        'answers': ['a) Black', 
-                    'b) Asian', 
+        'question': """What word did Dwight have taped to his
+        head for "Diversity Day":""",
+        'answers': ['a) Black',
+                    'b) Asian',
                     'c) Jewish'],
         'correct': 'b'
     },
     {
         'question': 'What is the perfume that Phylis wears made out of?:',
-        'answers': ['a) Cedar', 
-                    'b) Real Pine', 
+        'answers': ['a) Cedar',
+                    'b) Real Pine',
                     'c) Sandal Wood'],
         'correct': 'b'
     },
     {
-        'question': 'What is Gabes real name?:',
-        'answers': ['a) Susan', 
-                    'b) Gabriel', 
+        'question': 'What is Gabes last name?:',
+        'answers': ['a) Susan',
+                    'b) Gabriel',
                     'c) Lee'],
         'correct': 'a'
     },
     {
-        'question': 'What part of his body does Micheal burn on a George Foreman grill:',
-        'answers': ['a) Hand', 
-                    'b) Tongue', 
+        'question': """What part of his body does Micheal burn
+        on a George Foreman grill:""",
+        'answers': ['a) Hand',
+                    'b) Tongue',
                     'c) Foot'],
         'correct': 'c'
     },
     {
         'question': 'What serial killer is mentioned throughout the series:',
-        'answers': ['a) Scranton Strangler', 
-                    'b) Killer Kree', 
+        'answers': ['a) Scranton Strangler',
+                    'b) Killer Kree',
                     'c) Violent Vic'],
         'correct': 'a'
     },
     {
         'question': 'Where did Jim and Pam go on their honey moon?:',
-        'answers': ['a) Italy', 
-                    'b) Spain', 
+        'answers': ['a) Italy',
+                    'b) Spain',
                     'c) Puerto Rico'],
         'correct': 'c'
     },
     {
         'question': 'Who is Angela and Dwights sons godfather?:',
-        'answers': ['a) Senator Lipton', 
-                    'b) Oscar', 
+        'answers': ['a) Senator Lipton',
+                    'b) Oscar',
                     'c) Jim'],
         'correct': 'b'
     },
     {
         'question': 'Which of the following is a quote from Stanley?:',
-        'answers': ['a) Did I stutter?', 
-                    'b) Did I sneeze?', 
+        'answers': ['a) Did I stutter?',
+                    'b) Did I sneeze?',
                     'c) Did I shout?'],
         'correct': 'a'
     },
     {
         'question': 'What kind of animal got loose in the office?:',
-        'answers': ['a) Pigeon', 
-                    'b) Bat', 
+        'answers': ['a) Pigeon',
+                    'b) Bat',
                     'c) Cat'],
         'correct': 'b'
     },
     {
         'question': 'How many cats does Angela have:',
-        'answers': ['a) 13', 
-                    'b) 17', 
+        'answers': ['a) 13',
+                    'b) 17',
                     'c) 23'],
         'correct': 'b'
     },
     {
         'question': 'How many seasons was Micheal Scott on The Office:',
-        'answers': ['a) 5', 
-                    'b) 6', 
+        'answers': ['a) 5',
+                    'b) 6',
                     'c) 7'],
         'correct': 'c'
     },
     {
         'question': 'What was Stanleys wifes name',
-        'answers': ['a) Annie', 
-                    'b) Teri', 
+        'answers': ['a) Annie',
+                    'b) Teri',
                     'c) Cheryl'],
         'correct': 'b'
     },
     {
         'question': 'Where is The Office set?',
-        'answers': ['a) Scranton', 
-                    'b) Hunstvile', 
+        'answers': ['a) Scranton',
+                    'b) Hunstvile',
                     'c) The Woodlands'],
         'correct': 'a'
     },
     {
         'question': 'How many children did Jim and Pam have?:',
-        'answers': ['a) 2', 
-                    'b) 3', 
+        'answers': ['a) 2',
+                    'b) 3',
                     'c) 4'],
         'correct': 'a'
     },
     {
         'question': 'What was Dwights Farm named?:',
-        'answers': ['a) Schrute Farms', 
-                    'b) Beet Project', 
+        'answers': ['a) Schrute Farms',
+                    'b) Beet Project',
                     'c) Mo and Dwight'],
         'correct': 'a'
     },
     {
         'question': 'Where did Roy take Pam on their first date?:',
-        'answers': ['a) Minor league hockey game', 
-                    'b) The movies', 
+        'answers': ['a) Minor league hockey game',
+                    'b) The movies',
                     'c) A nice dinner'],
         'correct': 'a'
     },
     {
         'question': 'What is the name of Creeds blog?:',
-        'answers': ['a) My naughty list', 
-                    'b) Creed thoughts', 
+        'answers': ['a) My naughty list',
+                    'b) Creed thoughts',
                     'c) Dear Diary'],
         'correct': 'b'
     },
     {
         'question': 'What does Pam steal in the first season?:',
-        'answers': ['a) Staples', 
-                    'b) Post it notes', 
+        'answers': ['a) Staples',
+                    'b) Post it notes',
                     'c) Paper clips'],
         'correct': 'b'
     },
     {
         'question': 'Who dates Pams mother?',
-        'answers': ['a) Creed', 
-                    'b) Micheal', 
+        'answers': ['a) Creed',
+                    'b) Micheal',
                     'c) Toby'],
         'correct': 'b'
     },
     {
         'question': 'What does Dwights family farm produce?:',
-        'answers': ['a) Potatoes', 
-                    'b) Carrots', 
+        'answers': ['a) Potatoes',
+                    'b) Carrots',
                     'c) Beets'],
         'correct': 'c'
     },
     {
         'question': 'Who bought Micheals "Worlds Best Boss" mug:',
-        'answers': ['a) Dwight', 
-                    'b) Jim', 
+        'answers': ['a) Dwight',
+                    'b) Jim',
                     'c) Micheal'],
         'correct': 'c'
     },
     {
         'question': 'What food is Kevin forced to eat?',
-        'answers': ['a) Beets', 
-                    'b) Broccoli', 
+        'answers': ['a) Beets',
+                    'b) Broccoli',
                     'c) Potatoe'],
         'correct': 'b'
     },
     {
         'question': 'Where does Jim propose to Pam??',
-        'answers': ['a) A gas station', 
-                    'b) A fancy dinner', 
+        'answers': ['a) A gas station',
+                    'b) A fancy dinner',
                     'c) At the fireworks'],
         'correct': 'a'
     }
